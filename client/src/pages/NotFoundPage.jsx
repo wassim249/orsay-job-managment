5426d3a5108415dbf84889bcec04eb90a8243aca
Wassim249 <wassim.elbakkouri@yahoo.com>
2022-05-19 12:26:54 +0100

    add notfound page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import Layout from "../layout/Layout";
 
 export const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
+    <Layout>
+      <div className="flex flex-col  items-center justify-center h-screen">
+        <h1
+        
+        className="text-6xl text-primary font-bold">404</h1>
+        <span className="text-secondary">Page not found</span>
+      </div>
+    </Layout>
+  );
+};
