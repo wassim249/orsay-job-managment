33b563ced5d717317498635be4b042fd9efbb0af
wassim249 <wassim.elbakkouri@yahoo.com>
2022-05-30 11:03:42 +0100

    translate scans list

--- client/src/pages/scan/ScansListPage.jsx
+++ client/src/pages/scan/ScansListPage.jsx
@@ -8,13 +8,16 @@ import moment from "moment";
 import { scanSuccess } from "../../utils/Utils";
 import { useNavigate } from "react-router-dom";
 import { AlertContext } from "../../contexts/AlertContext";
+import LangContext from "../../contexts/LangContext";
+import LANG from "../../../../i18n/lang.json";
+import { Status } from "../../partials/Status";
 
 export const ScansListPage = () => {
   const [user] = useContext(UserContext);
   const [loading, setLoading] = useState(false);
   const [scans, setScans] = useState([]);
   const [alertData, setAlertData] = useContext(AlertContext);
-
+  const [lang] = useContext(LangContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -40,22 +43,22 @@ export const ScansListPage = () => {
     <Layout>
       {loading ? (
         <div className="w-full h-screen flex justify-center items-center">
-           {alertData && <AlertMessage />}
+          {alertData && <AlertMessage />}
           <SquareLoader color="#f88c6c" loading={loading} size="20px" />
         </div>
       ) : (
         <>
-          <div className="flex justify-between items-center">
-            <h1 className="text-2xl text-secondary font-bold   flex items-center">
-              <RiScan2Fill size={40} color="#f88c6c" className="mr-2" /> Scans
-              list
+          <div className="flex justify-between items-center w-full">
+            <h1 className="text-2xl text-secondary font-bold flex items-center">
+              <RiScan2Fill size={40} color="#f88c6c" className="mr-2" />
+              {LANG["scansList"]["Scans list"][lang]}
             </h1>
             {user?.role != "viewer" && (
               <button
                 className="bg-secondary text-white py-2 px-3  "
                 onClick={() => navigate("/scan/create")}
               >
-                + Create Scan
+                + {LANG["scansList"]["Create scan"][lang]}
               </button>
             )}
           </div>
@@ -63,12 +66,14 @@ export const ScansListPage = () => {
           <table className="table-auto mt-4 overflow-x-auto">
             <thead>
               <tr className="bg-primary text-white  font-medium text-center">
-                <th className="px-4 py-2">Date</th>
-                <th className="px-4 py-2">Source</th>
-                <th className="px-4 py-2">Destination</th>
-                <th className="px-4 py-2">Scanned orders</th>
-                <th className="px-4 py-2">Status</th>
-                <th className="px-4 py-2 bg-lightPrimary">Actions</th>
+                <th className=" py-2">Date</th>
+                <th className=" py-2">Source</th>
+                <th className=" py-2">Destination</th>
+                <th className=" py-2">
+                  {LANG["scansList"]["Scanned orders"][lang]}
+                </th>
+                <th className=" py-2">Status</th>
+                <th className=" py-2 bg-lightPrimary">Actions</th>
               </tr>
             </thead>
             <tbody>
@@ -87,16 +92,7 @@ export const ScansListPage = () => {
                       {scan && scan.orders.length}
                     </td>
                     <td className="border p-2 font-bold">
-                      {scan &&
-                        (scanSuccess(scan) ? (
-                          <span className="bg-green-500 text-white px-2 py-3">
-                            Success
-                          </span>
-                        ) : (
-                          <span className="bg-red-500 text-white px-2 py-3">
-                            Failed
-                          </span>
-                        ))}
+                      {scan && <Status success={scanSuccess(scan)} icon />}
                     </td>
                     <td className="border p-2">
                       <button
@@ -105,7 +101,7 @@ export const ScansListPage = () => {
                           navigate(`/scan/${scan.id}`);
                         }}
                       >
-                        View
+                        {LANG['common']['view'][lang]}
                       </button>
                     </td>
                   </tr>
